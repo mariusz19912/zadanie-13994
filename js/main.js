28199fe2cb579c8dc23ed3240a8b1bb12246a9c4
Mariusz Betkier <mariusz.betkier11@gmail.com>
2019-01-28 22:28:34 +0100

    poprawione

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
-function dodaj() {
-    document.getElementById("tekst").innerHTML="Akademia 108";
-};
-function usun() {
+document.getElementById("dodaj").addEventListener("click",function() {
+    document.getElementById("tekst").innerHTML = "Akademia 108";
+});
+document.getElementById("usun").addEventListener("click",function(){
     document.getElementById("tekst").innerHTML = "";
-};
+});
+
